5e69762af10a688960d3d6c927df6b542501495b
tranminhhieu <tranhieu200304@gmail.com>
2025-10-30 19:17:21 +0700

    message routes added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,7 +2,6 @@ import express from 'express'
 import { signup, login, logout, updateProfile } from '../controllers/auth.controller.js'
 import { protectRoute } from '../middleware/auth.middleware.js'
 import { arcjetProtection } from '../lib/arcjet.middleware.js'
-import arcjet from '@arcjet/node'
 
 const router = express.Router()
 
